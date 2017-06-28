d99b466e23054e6ffc1724ef747b270d8162d90f
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2017-06-28 11:54:22 +0200

    bytewise comparison because of broken encoding

--- scripts/validate_ids.sh
+++ scripts/validate_ids.sh
@@ -2,10 +2,10 @@
 
 
 FILENAME=$1
-IDS_DECLARED=`xpath $FILENAME '//role/@xml:id' 2> /dev/null | sed -E 's/xml:id="([a-zA-Z0-9-]+)"/\1/g' | tr ' ' '\n' | sort | uniq`
-IDS_USED=`xpath $FILENAME //sp/@who 2> /dev/null | sed -E 's/who="([a-zA-Z0-9-]*)"/\1/g' | tr ' ' '\n' | sort | uniq `
+IDS_DECLARED=`xpath $FILENAME '//role/@xml:id' 2> /dev/null | LC_ALL=C sed -E 's/xml:id="([^"]+)"/\1/g' | LC_ALL=C tr ' ' '\n' | LC_ALL=C sort | LC_ALL=C uniq`
+IDS_USED=`xpath $FILENAME //sp/@who 2> /dev/null | LC_ALL=C sed -E 's/who="([^"]*)"/\1/g' | LC_ALL=C tr ' ' '\n' | LC_ALL=C sort | LC_ALL=C uniq`
 echo "IDs used and not declared:"
-comm -3 -2  <(echo "$IDS_USED" ) <(echo "$IDS_DECLARED")
+LC_ALL=C comm -3 -2  <(echo "$IDS_USED" ) <(echo "$IDS_DECLARED")
 
 echo "IDs declared and not used:"
-comm -3 -1  <(echo "$IDS_USED" ) <(echo "$IDS_DECLARED")
+LC_ALL=C comm -3 -1  <(echo "$IDS_USED" ) <(echo "$IDS_DECLARED")
